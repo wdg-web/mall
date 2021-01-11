64788fdf7cb66fd3bfa84633bd62430ee441828d
wdg <wdg2571363943@outlook.com>
2021-01-11 14:42:05 +0800

    修改路由配置：history=>hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-const Home = () => import('views/home/Home')
-const Category = () => import('views/category/Category')
-const Cart = () => import('views/cart/Cart')
-const Profile = () => import('views/profile/Profile')
-const Detail = () => import('views/detail/Detail')
+const Home = () =>
+    import ('views/home/Home')
+const Category = () =>
+    import ('views/category/Category')
+const Cart = () =>
+    import ('views/cart/Cart')
+const Profile = () =>
+    import ('views/profile/Profile')
+const Detail = () =>
+    import ('views/detail/Detail')
 
 // 1.安装VueRouter
 Vue.use(VueRouter)
 
 // 2.配置路由信息
-const routes = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    component: Home
-  },
-  {
-    path: '/category',
-    component: Category
-  },
-  {
-    path: '/cart',
-    component: Cart
-  },
-  {
-    path: '/profile',
-    component: Profile
-  },
-  {
-    path: '/detail',
-    component: Detail
-  }
+const routes = [{
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        component: Home
+    },
+    {
+        path: '/category',
+        component: Category
+    },
+    {
+        path: '/cart',
+        component: Cart
+    },
+    {
+        path: '/profile',
+        component: Profile
+    },
+    {
+        path: '/detail',
+        component: Detail
+    }
 ]
 
 // 3.创建路由对象
 const router = new VueRouter({
-  mode: 'history',
-  routes
+    // mode: 'history',
+    routes
 })
 
 // 4.导出
