f692c1dcb91e56a2ecdb2b80e6099776a29d4226
wdg <wdg2571363943@outlook.com>
2021-01-11 13:17:20 +0800

    设置正确的 publicPath，添加deploy.sh文件

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'components': '@/components',
-        'content': 'components/content',
-        'common': 'components/common',
-        'assets': '@/assets',
-        'network': '@/network',
-        'views': '@/views',
-      }
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/main/' :
+        '/',
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'components': '@/components',
+                'content': 'components/content',
+                'common': 'components/common',
+                'assets': '@/assets',
+                'network': '@/network',
+                'views': '@/views',
+            }
+        }
     }
-  }
 }
